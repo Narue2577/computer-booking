08b3d11edfd311525684b65e1ba18f82ebd74250
Narue2577 <naarue2@gmail.com>
2025-07-16 17:47:01 +0700

    update home

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -84,6 +84,175 @@ export default function Home(){
   </div>
 </nav>
 
+<div data-hs-carousel='{
+    "loadingClasses": "opacity-0",
+    "dotsItemClasses": "hs-carousel-active:bg-blue-700 hs-carousel-active:border-blue-700 size-3 border border-gray-400 rounded-full cursor-pointer"
+  }' className="relative">
+  <div className="relative w-full overflow-hidden bg-white rounded-lg hs-carousel min-h-96">
+    <div className="absolute top-0 bottom-0 flex transition-transform duration-700 opacity-0 hs-carousel-body start-0 flex-nowrap">
+      <div className="hs-carousel-slide">
+        <div className="flex justify-center h-full p-6 bg-gray-100">
+          <span className="self-center text-4xl text-gray-800 transition duration-700">First slide</span>
+        </div>
+      </div>
+      <div className="hs-carousel-slide">
+        <div className="flex justify-center h-full p-6 bg-gray-200">
+          <span className="self-center text-4xl text-gray-800 transition duration-700">Second slide</span>
+        </div>
+      </div>
+      <div className="hs-carousel-slide">
+        <div className="flex justify-center h-full p-6 bg-gray-300">
+          <span className="self-center text-4xl text-gray-800 transition duration-700">Third slide</span>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <button type="button" className="absolute inline-flex items-center justify-center text-gray-800 -translate-y-1/2 bg-white border border-gray-100 rounded-full hs-carousel-prev hs-carousel-disabled:opacity-50 hs-carousel-disabled:cursor-default top-1/2 start-2 size-10 shadow-2xs hover:bg-gray-100 focus:outline-hidden">
+    <span className="text-2xl" aria-hidden="true">
+      <svg className="shrink-0 size-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+        <path d="m15 18-6-6 6-6"></path>
+      </svg>
+    </span>
+    <span className="sr-only">Previous</span>
+  </button>
+  <button type="button" className="absolute inline-flex items-center justify-center text-gray-800 -translate-y-1/2 bg-white border border-gray-100 rounded-full hs-carousel-next hs-carousel-disabled:opacity-50 hs-carousel-disabled:cursor-default top-1/2 end-2 size-10 shadow-2xs hover:bg-gray-100 focus:outline-hidden">
+    <span className="sr-only">Next</span>
+    <span className="text-2xl" aria-hidden="true">
+      <svg className="shrink-0 size-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+        <path d="m9 18 6-6-6-6"></path>
+      </svg>
+    </span>
+  </button>
+
+  <div className="absolute flex justify-center hs-carousel-pagination bottom-3 start-0 end-0 gap-x-2"></div>
+</div>
+
+
+
+<div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+    <table className="w-full text-sm text-left text-gray-500 rtl:text-right dark:text-gray-400">
+        <thead className="text-xs text-gray-700 uppercase dark:text-gray-400">
+            <tr>
+                <th scope="col" className="px-6 py-3 bg-gray-50 dark:bg-gray-800">
+                    Product name
+                </th>
+                <th scope="col" className="px-6 py-3">
+                    Color
+                </th>
+                <th scope="col" className="px-6 py-3 bg-gray-50 dark:bg-gray-800">
+                    Category
+                </th>
+                <th scope="col" className="px-6 py-3">
+                    Price
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr className="border-b border-gray-200 dark:border-gray-700">
+                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
+                    Apple MacBook Pro
+                </th>
+                <td className="px-6 py-4">
+                    Silver
+                </td>
+                <td className="px-6 py-4 bg-gray-50 dark:bg-gray-800">
+                    Laptop
+                </td>
+                <td className="px-6 py-4">
+                    $2999
+                </td>
+            </tr>
+            <tr className="border-b border-gray-200 dark:border-gray-700">
+                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
+                    Microsoft Surface Pro
+                </th>
+                <td className="px-6 py-4">
+                    White
+                </td>
+                <td className="px-6 py-4 bg-gray-50 dark:bg-gray-800">
+                    Laptop PC
+                </td>
+                <td className="px-6 py-4">
+                    $1999
+                </td>
+            </tr>
+            <tr className="border-b border-gray-200 dark:border-gray-700">
+                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
+                    Magic Mouse 2
+                </th>
+                <td className="px-6 py-4">
+                    Black
+                </td>
+                <td className="px-6 py-4 bg-gray-50 dark:bg-gray-800">
+                    Accessories
+                </td>
+                <td className="px-6 py-4">
+                    $99
+                </td>
+            </tr>
+            <tr className="border-b border-gray-200 dark:border-gray-700">
+                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
+                    Google Pixel Phone
+                </th>
+                <td className="px-6 py-4">
+                    Gray
+                </td>
+                <td className="px-6 py-4 bg-gray-50 dark:bg-gray-800">
+                    Phone
+                </td>
+                <td className="px-6 py-4">
+                    $799
+                </td>
+            </tr>
+            <tr>
+                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
+                    Apple Watch 5
+                </th>
+                <td className="px-6 py-4">
+                    Red
+                </td>
+                <td className="px-6 py-4 bg-gray-50 dark:bg-gray-800">
+                    Wearables
+                </td>
+                <td className="px-6 py-4">
+                    $999
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+
+
+
+<footer className="m-4 bg-white rounded-lg shadow-sm dark:bg-gray-900">
+    <div className="w-full max-w-screen-xl p-4 mx-auto md:py-8">
+        <div className="sm:flex sm:items-center sm:justify-between">
+            <a href="https://flowbite.com/" className="flex items-center mb-4 space-x-3 sm:mb-0 rtl:space-x-reverse">
+                <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
+                <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+            </a>
+            <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+                <li>
+                    <a href="#" className="hover:underline me-4 md:me-6">About</a>
+                </li>
+                <li>
+                    <a href="#" className="hover:underline me-4 md:me-6">Privacy Policy</a>
+                </li>
+                <li>
+                    <a href="#" className="hover:underline me-4 md:me-6">Licensing</a>
+                </li>
+                <li>
+                    <a href="#" className="hover:underline">Contact</a>
+                </li>
+            </ul>
+        </div>
+        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.</span>
+    </div>
+</footer>
+
+
         </>
     );
 }
