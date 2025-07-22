093992184d9cf01b4369f537253991e4ef9b6cca
Narue2577 <naarue2@gmail.com>
2025-07-22 10:02:23 +0700

    user profile

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -6,7 +6,6 @@ import Table from "../table/page";
 export default function Home(){
     return(
         <>
-
 <div className="w-full min-h-screen m-0 font-sans bg-gray-100">
 	<div className="bg-white shadow">
     <div className="container px-4 mx-auto">
@@ -24,6 +23,39 @@ export default function Home(){
           <a href="#" className="mr-4 text-sm font-semibold text-gray-800 hover:text-purple-600">About</a>
           <a href="#" className="mr-4 text-sm font-semibold text-gray-800 hover:text-purple-600">Logout</a>
           {/*<a href="#" className="text-sm font-semibold text-gray-800 hover:text-purple-600">Pricing</a> */}
+          <button data-popover-target="popover-user-profile" type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">User profile</button>
+
+{/*<div data-popover id="popover-user-profile" role="tooltip" className="absolute z-10 invisible inline-block w-64 text-sm text-gray-500 transition-opacity duration-300 bg-white border border-gray-200 rounded-lg shadow-xs opacity-0 dark:text-gray-400 dark:bg-gray-800 dark:border-gray-600">
+    <div className="p-3">
+        <div className="flex items-center justify-between mb-2">
+            <div>
+                <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-xs px-3 py-1.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Follow</button>
+            </div>
+        </div>
+        <p className="text-base font-semibold leading-none text-gray-900 dark:text-white">
+            <a href="#">Jese Leos</a>
+        </p>
+        <p className="mb-3 text-sm font-normal">
+            <a href="#" className="hover:underline">@jeseleos</a>
+        </p>
+        <p className="mb-4 text-sm">Open-source contributor. Building <a href="#" className="text-blue-600 dark:text-blue-500 hover:underline">flowbite.com</a>.</p>
+        <ul className="flex text-sm">
+            <li className="me-2">
+                <a href="#" className="hover:underline">
+                    <span className="font-semibold text-gray-900 dark:text-white">799</span>
+                    <span>Following</span>
+                </a>
+            </li>
+            <li>
+                <a href="#" className="hover:underline">
+                    <span className="font-semibold text-gray-900 dark:text-white">3,758</span>
+                    <span>Followers</span>
+                </a>
+            </li>
+        </ul>
+    </div>
+    <div data-popper-arrow></div>
+</div> */}
         </div>
 
         {/*<div className="hidden sm:flex sm:items-center">
